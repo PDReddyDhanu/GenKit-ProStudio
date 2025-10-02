fb0699b86551b8f1fc5614e0da65b9f5e72d5ded
P D Reddy Dhanu (Dhanunjay Reddy) <palakolanudhanunjayreddy@gmail.com>
2025-10-02 14:22:54 +0000

    in this page create the section

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -6,7 +6,7 @@ import { useHackathon } from '@/context/HackathonProvider';
 import Auth from './_components/Auth';
 import Dashboard from './_components/Dashboard';
 import PageIntro from '@/components/PageIntro';
-import { Code, Users, ClipboardList } from 'lucide-react';
+import { Code, Users, ClipboardList, GanttChartSquare } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import InvitationsManagement from './_components/InvitationsManagement';
 
@@ -34,15 +34,19 @@ export default function StudentPortal() {
                 <h1 className="text-3xl md:text-4xl font-bold font-headline">Student Portal</h1>
             </div>
              <Tabs defaultValue="dashboard" className="w-full">
-                <TabsList className="grid w-full h-auto grid-cols-1 sm:grid-cols-2">
+                <TabsList className="grid w-full h-auto grid-cols-1 sm:grid-cols-3">
                     <TabsTrigger value="dashboard"><Users className="mr-2"/> Dashboard</TabsTrigger>
                     <TabsTrigger value="management"><ClipboardList className="mr-2"/>My Teams & Requests</TabsTrigger>
+                    <TabsTrigger value="status"><GanttChartSquare className="mr-2"/>Project Status</TabsTrigger>
                 </TabsList>
                 <TabsContent value="dashboard" className="mt-6">
                     <Dashboard />
                 </TabsContent>
                 <TabsContent value="management" className="mt-6">
                     <InvitationsManagement />
+                </TabsContent>
+                 <TabsContent value="status" className="mt-6">
+                    <Dashboard />
                 </TabsContent>
             </Tabs>
         </div>
