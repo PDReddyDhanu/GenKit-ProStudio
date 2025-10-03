94f35fa2bc1ce37676e965dce1c8178c07dede59
P D Reddy Dhanu (Dhanunjay Reddy) <palakolanudhanunjayreddy@gmail.com>
2025-10-03 12:54:17 +0000

    For all types of registration pages and login pages design the that pro

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -163,6 +163,7 @@ export default {
       },
       animation: {
         'gradient': 'gradient 8s linear infinite',
+        'glowing-border': 'glowing-border 8s linear infinite',
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in': 'fade-in 0.5s ease-in-out forwards',
